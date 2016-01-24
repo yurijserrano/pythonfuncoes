85f1926ea63e2f22acf5783e724b4331ac7da887
Yuri <yurijserrano@gmail.com>
2016-01-24 18:43:10 -0200

    Primeira Função Python

---
+++ programa1.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+
+
+"""
+
+Utilização da função range
+
+
+Gera elementos de um determinando intervalo
+que podem ser jogados para uma lista.
+Ele vai do primeiro elemento até o último -1.
+
+
+"""
+
+
+
+
+lista = range(1,10)
+print lista
+
+lista2 = range(5,10)
+print lista2
